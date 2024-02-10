261258da7884af7d615642ea8735128c9b4d2e72
andoshin11 <shinglish11@gmail.com>
2024-02-10 09:59:07 +0900

    Setup DMTabs for event

--- src/DMTabs/DMTabs.ts
+++ src/DMTabs/DMTabs.ts
@@ -1,6 +1,5 @@
 import { LitElement, html } from "lit";
 import { customElement, property } from "lit/decorators.js";
-import "../loading";
 import { styleBase } from "./styles";
 import type { DMDeckData } from "../types";
 
@@ -9,94 +8,30 @@ export type Tab = "main" | "gr" | "hyperSpatial" | "dorumagedon" | "zeron";
 @customElement("dm-tabs")
 export class DMTabs extends LitElement {
   @property({ type: String })
-  currentTab: string = "main";
+  currentTab: Tab = "main";
 
   @property({ type: Object })
   deckData: DMDeckData | null = null;
-
-  @property({ type: Number })
-  mainCardsLength: number = 0;
-
-  @property({ type: Number })
-  grCardsLength: number = 0;
-
-  @property({ type: Number })
-  hyperSpatialCardsLength: number = 0;
-
-  @property({ type: Boolean })
-  hasDorumagedon: boolean = false;
-
-  @property({ type: Boolean })
-  hasZeron: boolean = false;
-
-  changeTab(e: Event) {
-    const target = e.target as HTMLElement;
-    const tab = target.dataset["tab"] as Tab;
-
-    if (this.currentTab === tab) return;
-    this.dispatchEvent(new CustomEvent("change", { detail: tab }));
-  }
 
   static styles = [styleBase];
 
   render() {
     return html`
       <div class="tabs">
-        <div
-          class="tab ${this.currentTab === "main" ? "active" : undefined}"
-          @click="${this.changeTab}"
-          data-tab="main"
-          data-testid="main-tab"
-        >
-          メイン ${this.deckData?.main_cards.length || 0}
+        <div class="tab" data-tab="main" data-testid="main-tab">メイン</div>
+        <div class="tab" data-tab="gr" data-testid="gr-tab">GR</div>
+        <div class="tab" data-tab="hyperSpatial" data-testid="hyperSpatial-tab">
+          超次元
         </div>
         <div
-          class="tab ${this.currentTab === "gr" ? "active" : undefined}"
-          @click="${this.changeTab}"
-          data-tab="gr"
-          data-testid="gr-tab"
+          class="tab"
+          data-tab="dorumagedon"
+          data-testid="dorumagedon-tab"
+          style="text-overflow: ellipsis; overflow: hidden; white-space: nowrap;"
         >
-          GR ${this.deckData?.gr_cards.length || 0}
+          ドルマゲドン
         </div>
-        <div
-          class="tab ${this.currentTab === "hyperSpatial"
-            ? "active"
-            : undefined}"
-          @click="${this.changeTab}"
-          data-tab="hyperSpatial"
-          data-testid="hyperSpatial-tab"
-        >
-          超次元 ${this.deckData?.hyper_spatial_cards.length || 0}
-        </div>
-        ${this.deckData?.dorumagedon
-          ? html`
-              <div
-                class="tab ${this.currentTab === "dorumagedon"
-                  ? "active"
-                  : undefined}"
-                @click="${this.changeTab}"
-                data-tab="dorumagedon"
-                data-testid="dorumagedon-tab"
-                style="text-overflow: ellipsis; overflow: hidden; white-space: nowrap;"
-              >
-                ドルマゲドン
-              </div>
-            `
-          : null}
-        ${this.deckData?.zeron
-          ? html`
-              <div
-                class="tab ${this.currentTab === "zeron"
-                  ? "active"
-                  : undefined}"
-                @click="${this.changeTab}"
-                data-tab="zeron"
-                data-testid="zeron-tab"
-              >
-                零龍
-              </div>
-            `
-          : null}
+        <div class="tab" data-tab="zeron" data-testid="zeron-tab">零龍</div>
       </div>
     `;
   }
